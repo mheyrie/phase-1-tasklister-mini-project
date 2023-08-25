4caf8dee3f4daf133bb07edaf1e59e8b7ebff199
Mary <omota4fidelity@gmail.com>
2023-08-25 11:02:16 +0100

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+const taskForm = document.querySelector("form")
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  handleInput(e.target.newtaskdescription.value)
+  taskForm.reset()  
+})
+})
+
+function handleInput(tas){
+  taskList = document.querySelector("#tasks");
+  let li = document.createElement("li");
+  let btn = document.createElement("button")
+  btn.addEventListener("click", del) 
+  btn.textContent = "X"
+  li.textContent = tas
+  li.appendChild(btn)
+  taskList.appendChild(li)
+ }
+ function del(e){
+  e.target.parentNode.remove()
+
+ }
